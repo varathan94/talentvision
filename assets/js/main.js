a0d836e0f23dac8cf25db33d99ec330ea51f4b48
karthik <karthikmurugavel09@outlook.com>
2026-09-13 10:53:08 +0400

    update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,12 @@
   "use strict";
 
  //disable inspecting
- document.addEventListener('contextmenu', e => e.preventDefault());
-document.addEventListener('keydown', e => {
-  if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
-    e.preventDefault();
-  }
-});
+//  document.addEventListener('contextmenu', e => e.preventDefault());
+// document.addEventListener('keydown', e => {
+//   if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+//     e.preventDefault();
+//   }
+// });
 
   function toggleScrolled() {
     const selectBody = document.querySelector('body');
